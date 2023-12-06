761506bb3a7700db4dcb77a710b9f90697315a7c
Abdullah Hussain <1abdullah.hussain05@gmail.com>
2023-12-06 23:27:49 +0000

    Commit #2, progress: Working CloseApp, Working Folder select, Colours corrected, fixed small mistake of having file select instead of folder select

--- FileOrganisation.py
+++ FileOrganisation.py
@@ -5,7 +5,9 @@ from customtkinter import filedialog
 
 customtk = customtkinter
 titleFont = ("Futura", 32, "bold")
-selected_dir = "Set to desired folder directory"
+navFont = ("arial", 32, "bold")
+pholderText = ("arial", 12, "italic")
+selected_dir = "-=-=( Please select your desired folder's directory )=-=-"
 
 class App(customtk.CTk):
     def __init__(self):
@@ -20,14 +22,17 @@ class App(customtk.CTk):
         self.labelTitle.grid(row=0, column=0, columnspan=2, pady=(10, 50))
         self.grid_columnconfigure(0, weight=1)
 
-        self.buttonSet_dir = customtk.CTkButton(self, text="Browse", bg_color="#181414")
+        self.buttonSet_dir = customtk.CTkButton(self, text="Browse", bg_color="#181414", corner_radius=0, border_width=0, fg_color="#302c34", hover_color="#201c24", command=self.browse_folder)
         self.buttonSet_dir.grid(row=1, column=0, padx=(50, 20), sticky="w")
 
-        self.entrydir = customtk.CTkEntry(self, placeholder_text=selected_dir, bg_color="#181414", placeholder_text_color="#fff")
+        self.entrydir = customtk.CTkLabel(self, text=selected_dir, bg_color="#181414", text_color="#fff", corner_radius=0, fg_color="#302c34", font=pholderText)
         self.entrydir.grid(row=1, column=0, sticky="ew", columnspan=2, padx=(200, 50))
+        self.entrydir.bind("<FocusIn>", self.clear_entry)
 
-
-
+        self.exitApp = customtk.CTkButton(self, text="x", text_color="#fff", font=navFont, width=25, corner_radius=0, fg_color="transparent", bg_color="#181414", hover_color="#181414", command=self.close_app)
+        self.exitApp.place(relx=.96, rely=.01)
+        self.exitApp.bind("<Enter>", lambda event: self.exitApp.configure(cursor="hand2"))
+        self.exitApp.bind("<Leave>", lambda event: self.exitApp.configure(cursor=""))
 
         self.bind("<ButtonPress-1>", self.start_drag)
         self.bind("<B1-Motion>", self.dragging)
@@ -44,10 +49,24 @@ class App(customtk.CTk):
         y = self.winfo_y() - self.y + event.y
         self.geometry(f"+{x}+{y}")
 
-    def browse_file():
-        file_path = filedialog.askopenfilename()
-        if file_path:
-            selected_dir = file_path
+    def on_enter(self, event):
+        self.exitApp.configure(cursor="hand2")  # Change cursor to a pointing hand
+
+    def on_leave(self, event):
+        self.exitApp.configure(cursor="")
+
+    def browse_folder(self):
+        global selected_dir
+        folder_path = filedialog.askdirectory()
+        selected_dir = folder_path
+        self.entrydir.configure(text="")  # Clear the entry widget
+        self.entrydir.configure(text=selected_dir)
+
+    def close_app(self):
+        self.destroy()
+
+    def clear_entry(self, event):
+        self.entrydir.delete(0, customtk.END)
 
 
 
